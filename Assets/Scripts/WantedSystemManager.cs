ae9bd58aa7514e133308966acb0ed9071b4a379f
ChickenGamer <rjmichalski2006@gmail.com>
2024-01-22 14:14:07 -0500

    Wave Increase
    The wave now increases when you call the function.

--- Assets/Scripts/WantedSystemManager.cs
+++ Assets/Scripts/WantedSystemManager.cs
@@ -11,15 +11,17 @@ public class WantedSystemManager : MonoBehaviour
     
     public int firstWaveMinimumEnemyCount = 1;
     public int firstWaveMaximumEnemyCount = 5;
+
+    public float enemySpawnIncreaseMinimum = 1.25;
+    public float enemySpawnIncreaseMaximum = 2;
+
+    private int currentWave = 1;
+
+    public int enemiesKilled = 0;
     
     private int percentToStar;
 
     [SerializeField] private List<GameObject> antiCommunistList;
-
-    private void Awake()
-    {
-        StartHunt();
-    }
 
     public void WantedLevelCalculation()
     {
@@ -32,21 +34,42 @@ public class WantedSystemManager : MonoBehaviour
             StartHunt();
         }
     }
+
+    public int CurrentEnemyCount;
     private void StartHunt()
     {
         int randomAntiCommunistCount = Random.Range(firstWaveMinimumEnemyCount, firstWaveMaximumEnemyCount);
 
+        CurrentEnemyCount = randomAntiCommunistCount;
+        
         for (int currentEnemyCount = 0; currentEnemyCount < randomAntiCommunistCount; currentEnemyCount++)
         {
             int randomAntiCommunist = Random.Range(0, antiCommunistList.Count);
 
-            float randomX = InclusiveRandom(1.5f, 5f);
-            float randomY = InclusiveRandom(1.5f, 5f);
+            float randomX = InclusiveRandom(1.5f, 2.5f);
+            float randomY = InclusiveRandom(1.5f, 2.5f);
             
             Vector2 outsideCameraSpawnpoint = Camera.main.ViewportToWorldPoint(new Vector3(randomX, randomY, 0f));
             
             Instantiate(antiCommunistList[randomAntiCommunist], outsideCameraSpawnpoint, Quaternion.identity);
         }
+    }
+
+    public void CheckEnemiesKilled()
+    {
+        if (enemiesKilled >= CurrentEnemyCount)
+        {
+            WaveIncrease(CurrentEnemyCount);
+        }
+    }
+    private int WaveIncrease(int currentEnemyCount)
+    {
+        currentWave += 1;
+        enemiesKilled = 0;
+        
+        float randomIncreaseAmount = Random.Range(enemySpawnIncreaseMinimum, enemySpawnIncreaseMaximum);
+
+        return currentEnemyCount * (int) Mathf.Ceil(randomIncreaseAmount);
     }
     
     private float InclusiveRandom(float min, float max)
