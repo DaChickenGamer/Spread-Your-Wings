141a9ceb1a405a0ff0cfacf93801d986719dfa7f
ChickenGamer <rjmichalski2006@gmail.com>
2024-01-24 13:10:39 -0500

    Egg Hatching
    There is now egg hatching when you start the game,

---
+++ Assets/Scripts/Object Scripts/Egg Hatching.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro.EditorUtilities;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
+
+public class EggHatching : MonoBehaviour
+{
+    [SerializeField] private List<Sprite> eggStages;
+    [SerializeField] private GameObject eggPrefab;
+    [SerializeField] private GameObject middleOfTheScreen;
+    
+    private PlayerMovement playerMovement;
+
+    private GameObject currentEgg;
+    
+    private int currentEggStage;
+    private void Awake()
+    {
+        playerMovement = FindObjectOfType<PlayerMovement>();
+        
+        if (SceneManager.GetActiveScene().buildIndex == SceneManager.GetSceneByName("Main Scene").buildIndex)
+        {
+            playerMovement.shouldMove = false;
+            StartHatching();
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            NextEgg();
+        }
+    }
+
+    private void StartHatching()
+    {
+        currentEggStage = 0;
+        currentEgg = Instantiate(eggPrefab, new Vector3(middleOfTheScreen.transform.position.x, middleOfTheScreen.transform.position.x, 0), Quaternion.identity);
+    }
+
+    private void NextEgg()
+    {
+        if (currentEggStage == eggStages.Count)
+        {
+            EndHatching();
+            return;
+        }
+
+        currentEgg.GetComponent<SpriteRenderer>().sprite = eggStages[currentEggStage];
+        currentEggStage += 1;
+    }
+    
+    private void EndHatching()
+    {
+        playerMovement.shouldMove = true;
+        Destroy(currentEgg);
+    }
+}
