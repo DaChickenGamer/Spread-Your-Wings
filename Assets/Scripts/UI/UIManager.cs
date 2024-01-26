3d83821d77b73a4111f3e47df44f817ca943b8d5
ChickenGamer <rjmichalski2006@gmail.com>
2024-01-25 21:20:37 -0500

    Working Timer & Combined UI Manager
    I combined the UI manager with display stats. In addition I made a timer for the game so the player now has a reason to repeat playthroughs.

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -10,6 +10,11 @@ public class UIManager : MonoBehaviour
 {
     public Slider slider;
     private TextMeshProUGUI progressText;
+
+    public GameObject timerPanel;
+    
+    [SerializeField] private TextMeshProUGUI communismText;
+    [SerializeField] private TextMeshProUGUI followersText;
     
     public static UIManager instance { get; private set; }
     
@@ -22,7 +27,78 @@ public class UIManager : MonoBehaviour
 
         instance = this;
         
-        progressText = GetComponentInChildren<TextMeshProUGUI>();
+        progressText = GetComponentInChildren<TextMeshProUGUI>(); }
+
+    private void Start()
+    {
+        StartCoroutine(GameTimer());
+    }
+    
+    void Update()
+    {
+        UpdateCommunismText();
+        UpdateFollowersText();
+    }
+
+    private void UpdateCommunismText()
+    {
+        communismText.text = AbbreviateNumber(CommunismSystem.instance.GetCommunism());
+    }
+    private void UpdateFollowersText()
+    {
+        followersText.text = AbbreviateNumber(CommunismSystem.instance.GetFollowers());
+    }
+
+    private string AbbreviateNumber(int numberToAbbreviate)
+    {
+        string[] suffixes = { "", "K", "M", "B", "T"};
+        int index = 0;
+
+        while (numberToAbbreviate >= 1000 && index < suffixes.Length - 1)
+        {
+            numberToAbbreviate /= 1000;
+            index++;
+        }
+
+        if (index > 0)
+        {
+            return numberToAbbreviate.ToString() + suffixes[index];
+        }
+        else
+        {
+            return numberToAbbreviate.ToString();
+        }
+    }
+
+    public IEnumerator GameTimer()
+    {
+        TextMeshProUGUI timerText = timerPanel.GetComponentInChildren<TextMeshProUGUI>();
+
+        int hours = 0;
+        int minutes = 0;
+        int seconds = 0;
+        
+        bool timeRunning = true;
+        
+        while (timeRunning)
+        {
+            seconds++;
+            
+            if (seconds >= 60)
+            {
+                minutes++;
+                seconds = 0;
+            }
+            if (minutes >= 60)
+            {
+                hours++;
+                minutes = 0;
+            }
+
+            timerText.text = hours.ToString("00") + ":" + minutes.ToString("00") + ":" + seconds.ToString("00");
+            
+            yield return new WaitForSeconds(1);
+        }
     }
     public void SetMaxProgress(int progress)
     {
